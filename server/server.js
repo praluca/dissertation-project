29d78a5bd36fbe5d3d27733b8e2e20ed30987367
Popa Raluca <poparaluca98@yahoo.com>
2022-06-04 14:49:24 +0300

    set database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,20 @@
-const express = require('express')
-const port = 3000
+const express = require("express");
+const connectDB = require("./config/db");
+const port = 3000;
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// connect to database
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+// define routes
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
