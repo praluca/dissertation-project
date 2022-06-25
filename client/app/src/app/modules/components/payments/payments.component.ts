c13f331d21166a70fe7d148c5b57cf64513db395
Popa Raluca <poparaluca98@yahoo.com>
2022-06-25 10:57:42 +0300

    added FE  for the last 2 screens

--- client/app/src/app/modules/components/payments/payments.component.ts
+++ client/app/src/app/modules/components/payments/payments.component.ts
@@ -1,15 +1,103 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { DeleteDialogComponent } from 'src/app/shared/delete-dialog/delete-dialog.component';
+const ELEMENT_DATA: any = [
+  {
+    speciality: 'Cardiologie',
+    category: 'Investigatie laborator',
+    type: 'Medicina Muncii',
+    location: 'Screening',
+    price: '110',
+  },
+  {
+    speciality: 'Cardiologie',
+    category: 'Consultatie clinica',
+    type: 'Interventii Chirurgicale',
+    location: 'Screening',
+    price: '200',
+  },
+  {
+    speciality: 'Cardiologie',
+    category: 'Consultatie clinica',
+    type: 'Proceduri medicale',
+    location: 'Screening',
+    price: '500',
+  },
+  {
+    speciality: 'Cardiologie',
+    category: 'Consultatie clinica',
+    type: 'Medicina Muncii',
+    location: 'Screening',
+    price: '100',
+  },
+  {
+    speciality: 'Cardiologie',
+    category: 'Consultatie clinica',
+    type: 'Medicina Muncii',
+    location: 'Screening',
+    price: '150',
+  },
+  {
+    speciality: 'Cardiologie',
+    category: 'Investigatie laborator',
+    type: 'Imunologie',
+    location: 'Screening',
+    price: '150',
+  },
+  {
+    speciality: 'Cardiologie',
+    category: 'Investigatie laborator',
+    type: 'Proceduri medicale',
+    location: 'Screening',
+    price: '1000',
+  },
+  {
+    speciality: 'Cardiologie',
+    category: 'Investigatie laborator',
+    type: 'TOXICOLOGIE',
+    location: 'Screening',
+    price: '100',
+  },
+];
 
 @Component({
   selector: 'app-payments',
   templateUrl: './payments.component.html',
-  styleUrls: ['./payments.component.scss']
+  styleUrls: ['./payments.component.scss'],
 })
 export class PaymentsComponent implements OnInit {
+  displayedColumns: string[] = [
+    'speciality',
+    'category',
+    'type',
+    'location',
+    'price',
+  ];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
+  searchText: any;
+  initialData: any;
 
-  constructor() { }
+  constructor(public dialog: MatDialog) {}
 
   ngOnInit(): void {
+    this.initialData = this.dataSource;
   }
 
+  onSearch() {
+    console.log('title', this.searchText);
+    this.dataSource.filter = this.searchText.trim().toLowerCase();
+  }
+  onSearchInput(event: any) {
+    console.log('title_event', event.target.value);
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  deleteConsultation() {
+    const dialogRef = this.dialog.open(DeleteDialogComponent, {
+      width: '400px',
+      height: '200px',
+      data: { name: 'Raluca' },
+    });
+  }
 }
--- client/app/src/app/modules/components/shop/shop.component.ts
+++ client/app/src/app/modules/components/shop/shop.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.scss']
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent implements OnInit {
+  panelOpenState = true;
+  panelOpenState2 = true;
+  toppings = this._formBuilder.group({
+    pepperoni: false,
+    extracheese: false,
+    mushroom: false,
+  });
+  searchText: any = '';
+  isClick = false;
 
-  constructor() { }
+  constructor(private _formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSearch() {
+    console.log('search');
   }
-
+  onSearchInput(event: any) {
+    console.log('search input');
+  }
 }
